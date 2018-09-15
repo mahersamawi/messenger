fe8430846aa638d85bee0ede0ccc9bd55bd5ced2
Maher Samawi <mahersamawi@yahoo.com>
2018-09-15 16:02:29 -0700

    Added styles and event listeners.

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,38 +1,82 @@
 import React, { Component } from 'react';
 import './App.css';
 import io from 'socket.io-client';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Col } from 'reactstrap';
 
 var ip = "192.168.1.2";
 var socket = io('http://' + ip + ':5000');
-
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       room: "",
-      text: ""
+      text: "",
+      old_messages : []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  componentDidMount(){
-      socket.on('connect', function() {
-      var send_dict = {};
-      send_dict['msg'] = "User has connected!";
-      send_dict['room'] = "None";
-      socket.send(send_dict);
-    });
+    this.makeList = this.makeList.bind(this);
+    this.addToList = this.addToList.bind(this);
   }
 
+
+  addToList(msg){
+    console.log("Before state");
+    var arr = this.state.old_messages.push(msg);
+    console.log("Settng state");
+    this.setState({old_messages: arr});
+  }
+
+
+  makeList(list){
+    const listItems = list.map((list) =>
+      <li>{list}</li>
+    );
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+
+
+  componentDidMount(){
+    socket.on('connect', function() {
+    var send_dict = {};
+      send_dict['msg'] = "User has connected!";
+      send_dict['room'] = "None";
+      send_dict['time'] = Date.now();
+      socket.send(send_dict);
+    });
+    // just for testing 
+    socket.emit("join", 'poop'); 
+    socket.on('message', function(msg) {
+      console.log('Received message');
+      console.log(msg);
+      console.log(this);
+    });
+    let url = "http://192.168.1.2:5000/room_messages";
+    
+
+    return fetch(url)
+      .then((response) => 
+        response.json()
+        .then((responseJson) => {
+          console.log(responseJson.messsages);
+          this.setState({ old_messages: responseJson.messsages })
+        })
+        .catch((error) => {
+          console.log(error);
+          })
+        )
+  }
+    
   handleSubmit(event){
     console.log("send it");
     event.preventDefault();
     var send_dict = {};
     send_dict['msg'] = this.state.text;
-    send_dict['room'] = this.state.room;
+    send_dict['room'] = "poop";
+    send_dict['time'] = Date.now();
     socket.send(send_dict);
     this.setState(
     {
@@ -55,7 +99,14 @@ class App extends Component {
           <p> room </p>
         </Col>
         <Col className="Text">
-          <p> text </p>
+          <h1> text </h1>
+          <ul>{this.makeList(this.state.old_messages)}</ul>,
+          <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" value={this.state.text} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         </Col>
         <Col className="Members">
           <p> members </p>
